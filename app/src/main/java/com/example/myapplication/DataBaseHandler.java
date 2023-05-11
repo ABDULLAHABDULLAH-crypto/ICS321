eafe5d3ae3dddb3249a42ebe0d46990fa776bfe8
Abdullah <abod098.a.d@gmail.com>
2023-05-11 10:52:09 +0300

    update the user page and DataBaseHandler

--- app/src/main/java/com/example/myapplication/DataBaseHandler.java
+++ app/src/main/java/com/example/myapplication/DataBaseHandler.java
@@ -6,17 +6,37 @@ import android.database.sqlite.SQLiteDatabase;
 import androidx.annotation.Nullable;
 
 public class DataBaseHandler  extends android.database.sqlite.SQLiteOpenHelper {
-    public DataBaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private Context context ;
+    private static final String DATABASE_NAME = "tournament";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "tournament";
+    private static final String tr_id = "tr_id";
+    private static final String tr_name = "tr_name";
+    private static final String start_date = "start_date";
+    private static final String end_date = "end_date";
+
+    public DataBaseHandler(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String query_tournament =
+                "CREATE TABLE " + TABLE_NAME +
+                        " (" + tr_id + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                                tr_name + " TEXT, " +
+                                start_date + " TEXT, " +
+                                end_date + " TEXT);";
+        db.execSQL(query_tournament);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView ;
+    Button redCard;
+    Button highestP;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        redCard = findViewById(R.id.btnPlayerRedCard);
+        highestP = findViewById(R.id.btnPlayerWithHighsetGoal);
+
     }
 }
