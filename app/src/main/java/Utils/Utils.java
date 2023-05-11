f02f4959f860a94a883cea37a287c18f3c8daf69
ABDULLAHABDULLAH-crypto <74300140+ABDULLAHABDULLAH-crypto@users.noreply.github.com>
2023-05-11 23:15:25 +0300

    Update Utils.java
    this will have all variables

--- app/src/main/java/Utils/Utils.java
+++ app/src/main/java/Utils/Utils.java
@@ -2,5 +2,44 @@ package Utils;
 
 public class Utils {
 
+// Venue table
+public static String Venue_Table="plyer";
+    public static String main_venue_id="plyer_id";
+
+
+/// Player table variables
+    public static String Plyer_Table="plyer";
+    public static String plyer_id="plyer_id";
+    public static  String team_id = "team_id";
+    public static  String jersey_no = "jersey_no";
+    public static  String player_name = "player_name";
+    public static String position_to_play="position_to_play";
+    public static String dt_of_bir="dt_of_bir";
+
+
+// referee Table
+    public static String refree_Table="referee";
+    public static String main_referee_id="referee_id";
+    public static String referee_name="referee_name";
+
+
+//  match_played Table
+    public static String  match_played_table=" match_played";
+    public static String match_no="match_no";
+    public static String play_stage="play_stage";
+    public static  String play_date = "play_date";
+    public static  String results = "results";
+    public static  String decided_by = "decided_by";
+    public static String goal_score="goal_score";
+    public static String venue_id="venue_id";
+    public static  String match_referee_id = "match_referee_id";
+    public static  String audience = "audience";
+    public static  String player_of_match = "player_of_match";
+    public static String stop1_sec="stop1_sec";
+    public static String stop2_sec="stop2_sec";
+//    Coach Table
+    public static String  coach_table=" coach";
+    public static String coach_id="coach_id";
+    public static String coach_name="coach_name";
 
 }
