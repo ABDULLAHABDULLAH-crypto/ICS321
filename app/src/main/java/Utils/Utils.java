e2fb60f0d1e9db53755e8ed2499600db3314eb4d
ABDULLAHABDULLAH-crypto <74300140+ABDULLAHABDULLAH-crypto@users.noreply.github.com>
2023-05-12 10:50:27 +0300

    Update Utils.java
    New tables

--- app/src/main/java/Utils/Utils.java
+++ app/src/main/java/Utils/Utils.java
@@ -3,18 +3,24 @@ package Utils;
 public class Utils {
 
 // Venue table
-public static String Venue_Table="plyer";
+    public static String Venue_Table="plyer";
     public static String main_venue_id="plyer_id";
+    public static String venue_name="plyer_id";
+    public static String venue_status="venue_status";
+    public static String aud_capacity="aud_capacity";
 
 
-/// Player table variables
+
+
+    /// Player table variables
     public static String Plyer_Table="plyer";
     public static String plyer_id="plyer_id";
-    public static  String team_id = "team_id";
+    public static  String team_id_plyer = "team_id";
     public static  String jersey_no = "jersey_no";
     public static  String player_name = "player_name";
     public static String position_to_play="position_to_play";
     public static String dt_of_bir="dt_of_bir";
+
 
 
 // referee Table
@@ -25,7 +31,7 @@ public static String Venue_Table="plyer";
 
 //  match_played Table
     public static String  match_played_table=" match_played";
-    public static String match_no="match_no";
+    public static String main_match_no="match_no";
     public static String play_stage="play_stage";
     public static  String play_date = "play_date";
     public static  String results = "results";
@@ -41,5 +47,41 @@ public static String Venue_Table="plyer";
     public static String  coach_table=" coach";
     public static String coach_id="coach_id";
     public static String coach_name="coach_name";
+
+//  asst_referee_Table
+    public static String  asst_referee_Table=" asst_referee";
+    public static String asst_ref_id="asst_ref_id";
+
+//    match_details_Table
+    public static String match_details_Table="match_details";
+    public static String match_no_match_details="match_no";
+    public static String team_id_match_details="team_id";
+    public static String play_stage_match_details="play_stage";
+    public static String match_no = "match_no";
+    public static  String team_id_match = "team_id";
+    public static String win_lose="win_lose";
+    public static  String penalty_score = "penalty_score";
+    public static  String asst_ref = "asst_ref";
+    public static String player_gk="player_gk";
+
+//    goal_details
+
+    public static String goal_details_table="goal_details";
+    public static String goal_id = "goal_id";
+    public static String match_no_goal_details="match_no";
+    public static  String player_id_goal_details = "player_id";
+    public static String team_id_goal_details="team_id";
+    public static  String goal_time = "goal_time";
+    public static  String goal_type = "goal_type";
+    public static String play_stage_goal_details="play_stage";
+    public static  String goal_schedule = "goal_schedule";
+    public static  String goal_half = "goal_half";
+
+
+//  penalty_shootout Table
+
+
+
+
 
 }
